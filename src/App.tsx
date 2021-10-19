3f3cfdefcb51d69038859aa70fe282cbfc60596c
artyom-bashkatov <artyom.bashkatov@distillery.com>
2021-10-19 15:09:13 +0300

    types, interfaces, generics and synthetic events, declare variable in TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,37 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 // import TodoForm from './components/TodoForm';
 import TodoFormRef from './components/TodoFormRef';
+import TodoList from './components/TodoList';
+import { ITodo } from './interfaces';
+
+declare var confirm: (question: string) => boolean;
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const addHandler = (title: string) => {
-    console.info('Add new ToDo', title);
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    };
+    setTodos(prev => [newTodo, ...prev])
+  }
+
+  const toggleHandler = (id: number) => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }));
+  }
+
+  const removeHandler = (id: number) => {
+    const shouldRemove = confirm("Уверены, что хотите удалить элемент?");
+    if (shouldRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
   }
 
   return (
@@ -15,6 +40,7 @@ const App: React.FC = () => {
     <Navbar />
     <div className="container">
       <TodoFormRef onAdd={addHandler} />
+      <TodoList todos={todos} onToggle={toggleHandler} onRemove={removeHandler} />
     </div>
     </>
   );
